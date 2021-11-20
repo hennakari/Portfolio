684352bb8718489720e641c62e974b0cfb170fcc
Henna Kari <AB5148@student.jamk.fi>
2021-11-20 20:46:13 +0200

    CSS, text content and images added.

---
+++ dist/js/main.js
@@ -0,0 +1,35 @@
+//Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//Set Initial State of Menu
+let showMenu = false;
+
+//when we click, it gets the class of close
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu () { //to check the state of menu
+    if(!showMenu) { //if the menu is NOT shown
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //reset the menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //reset the menu State
+        showMenu = false;
+
+    }
+}
